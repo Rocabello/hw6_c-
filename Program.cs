e280709a08b00730a8c44d88dd451bdd1e2db33c
Ro <adm.031@ya.ru>
2023-03-30 19:19:43 +0300

    Задача 1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите числа через пробел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+Console.WriteLine(SumOfEven(array));
+
+// ----------Метод---------------
+int SumOfEven(int[] newArray)
+{   
+    int count =0;
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        if (newArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
